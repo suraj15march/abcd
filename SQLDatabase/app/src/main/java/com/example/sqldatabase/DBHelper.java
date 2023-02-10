4d53a94568a1413d02ebad2506c8a7826c08a3c6
Suraj Kumar <61618872+suraj15march@users.noreply.github.com>
2023-02-10 14:12:48 +0530

    Update DBHelper.java

--- SQLDatabase/app/src/main/java/com/example/sqldatabase/DBHelper.java
+++ SQLDatabase/app/src/main/java/com/example/sqldatabase/DBHelper.java
@@ -1,12 +1,16 @@
 package com.example.sqldatabase;
 
-import android.content.ContentValues; import android.content.Context; import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase; import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
-public class DBHelper extends SQLiteOpenHelper { public DBHelper(@Nullable Context context){
-    super(context, "userdata.db",null,1);
-}
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "userdata.db", null, 1);
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create Table Userdetails(name TEXT primary key,contact TEXT, age TEXT)");
@@ -14,37 +18,60 @@ public class DBHelper extends SQLiteOpenHelper { public DBHelper(@Nullable Conte
 
     @Override
 
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { db.execSQL("drop Table if exists Userdetails");
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop Table if exists Userdetails");
     }
 
-    public boolean insertdata(String name,String contact, String age){ SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues(); contentValues.put("name",name); contentValues.put("contact",contact); contentValues.put("age",age);
-        long result = db.insert("Userdetails",null,contentValues); if(result == -1)
-            return false; else return true;
+    public boolean insertdata(String name, String contact, String age) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("contact", contact);
+        contentValues.put("age", age);
+        long result = db.insert("Userdetails", null, contentValues);
+        if (result == -1)
+            return false;
+        else return true;
     }
 
-    public boolean updateData(String name,String contact,String age){ SQLiteDatabase db = this.getWritableDatabase(); ContentValues contentValues = new ContentValues(); contentValues.put("contact",contact); contentValues.put("age",age);
-        Cursor cursor = db.rawQuery("Select * from Userdetails where name=?",new String[]{name});
-        if(cursor.getCount()>0)
-        {
-            long result = db.update("Userdetails",contentValues,"name=?",new String[]{name});
-            if(result==-1) return false;
+    public boolean updateData(String name, String contact, String age) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("contact", contact);
+        contentValues.put("age", age);
+        Cursor cursor = db.rawQuery("Select * from Userdetails where name=?", new String[] {
+            name
+        });
+        if (cursor.getCount() > 0) {
+            long result = db.update("Userdetails", contentValues, "name=?", new String[] {
+                name
+            });
+            if (result == -1) return false;
             else return true;
-        }else return false;
+        } else return false;
 
     }
 
-    public boolean deleteData(String name){ SQLiteDatabase db = this.getWritableDatabase(); ContentValues contentValues = new ContentValues();
-        Cursor cursor = db.rawQuery("Select * from Userdetails where name=?",new String[]{name});
-        if(cursor.getCount()>0){
-            long result = db.delete("Userdetails","name=?",new String[]{name}); if(result==-1)
-                return false; else return true;
-        }else return false;
+    public boolean deleteData(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        Cursor cursor = db.rawQuery("Select * from Userdetails where name=?", new String[] {
+            name
+        });
+        if (cursor.getCount() > 0) {
+            long result = db.delete("Userdetails", "name=?", new String[] {
+                name
+            });
+            if (result == -1)
+                return false;
+            else return true;
+        } else return false;
     }
 
-    public Cursor viewData(){
+    public Cursor viewData() {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("select * from Userdetails",null); return cursor;
+        Cursor cursor = db.rawQuery("select * from Userdetails", null);
+        return cursor;
     }
 }
